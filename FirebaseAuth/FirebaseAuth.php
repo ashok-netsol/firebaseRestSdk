b98e1a1f77a6c9df698036a11c7dfe7877898a41
ashok.kumar <ashok.kumar.1@netsolutions.com>
2023-11-17 09:38:31 +0530

    Initial commit 3

---
+++ FirebaseAuth/FirebaseAuth.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace FirebaseAuth;
+
+
+use GuzzleHttp\Client;
+
+class FirebaseAuthSDK 
+{
+    private static $baseUrl = 'https://identitytoolkit.googleapis.com/v1/';
+    private static $apiKey;
+
+
+    public static function initialize($apiKey)
+    {
+        self::$apiKey = $apiKey;
+    }
+
+    private static function getClient()
+    {
+        return new Client();
+    }
+
+    public static function register($email, $password)
+    {
+        $client = self::getClient();
+        $response = $client->post(self::$baseUrl . 'accounts:signUp?key=' . self::$apiKey, [
+            'json' => [
+                'email' => $email,
+                'password' => $password,
+                // Add other parameters as needed
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    public static function login($email, $password)
+    {
+        $client = self::getClient();
+        $response = $client->post(self::$baseUrl . 'accounts:signInWithPassword?key=' . self::$apiKey, [
+            'json' => [
+                'email' => $email,
+                'password' => $password,
+                // Add other parameters as needed
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    public static function update($idToken, $newData)
+    {
+        $client = self::getClient();
+        $response = $client->post(self::$baseUrl . 'accounts:update?key=' . self::$apiKey, [
+            'json' => [
+                'idToken' => $idToken,
+                'displayName' => $newData['displayName'], // Example: Update display name
+                // Add other parameters as needed
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    public static function sendResetPasswordEmail($email)
+    {
+        $client = self::getClient();
+        $response = $client->post(self::$baseUrl . 'accounts:sendOobCode?key=' . self::$apiKey, [
+            'json' => [
+                'email' => $email,
+                'requestType' => 'PASSWORD_RESET',
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+}
+
